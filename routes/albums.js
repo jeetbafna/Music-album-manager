8f4a024980c162a96ef8798241ffb093ac370290
jeet bafna <jeetanil97@gmail.com>
2018-01-08 00:35:17 +0530

    adding new albums via firebase

--- routes/albums.js
+++ routes/albums.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
+var Firebase = require('firebase');
+var fbRef = new Firebase('https://albumz-f5975.firebaseio.com/');
+var multer = require('multer');
+var upload = multer({dest:'./public/images/uploads'});
 
 //Home page
 router.get('/', function(req, res, next){
 	res.render('albums/index');
+});
+
+router.get('/add', function(req, res, next){
+	var genreRef = fbRef.child('genres');
+
+	genreRef.once('value', function(snapshot){
+		var data = [];
+		snapshot.forEach(function(childSnapshot){
+			var key = childSnapshot.key();
+			var childData = childSnapshot.val();
+			data.push({
+				id: key,
+				name: childData.name
+			});
+		});
+		res.render('albums/add', {genres: data});
+	});
+	
+});
+
+router.post('/add', upload.single('cover'), function(req, res, next){
+	//CHeck file Upload
+	if(req.file){
+		console.log('Uploading File...');
+		var cover = req.file.filename;
+	} else{
+		console.log('No file uploaded');
+		var cover = 'noimage.jpg';
+	}
+
+	//Build album object
+	var album = {
+		artist: req.body.artist,
+		title: req.body.title,
+		genre: req.body.genre,
+		info: req.body.info,
+		year: req.body.year,
+		label: req.body.label,
+		tracks: req.body.tracks,
+		cover: cover
+	};
+
+	//Create ref
+	var albumRef = fbRef.child('albums');
+	albumRef.push().set(album);
+
+	req.flash('success_msg', 'Album Saved');
+	res.redirect('/albums');
+
 });
 
 
